fbabc455b0df18a90136560bf26ddc1d88868edd
João Vitor Gomes <jvictor.gomes@outlook.com>
2024-09-29 04:37:40 -0300

    29/09/2025
    melhora da pagina de registro

--- src/app/pages/register-page/register/register.page.ts
+++ src/app/pages/register-page/register/register.page.ts
@@ -8,28 +8,66 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage {
-  registerForm: FormGroup;
+  currentStep = 1;
+  registerFormStep1: FormGroup;
+  registerFormStep2: FormGroup;
   showPassword = false;
 
   constructor(private fb: FormBuilder, private router: Router) {
-    this.registerForm = this.fb.group({
+    // Formulário da primeira etapa
+    this.registerFormStep1 = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(3)]],
       phoneNumber: ['', [Validators.required, Validators.pattern(/^[0-9]{10,}$/)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      acceptTerms: [false, Validators.requiredTrue],
+      acceptTerms: [false, Validators.requiredTrue]
+    });
+
+    // Formulário da segunda etapa
+    this.registerFormStep2 = this.fb.group({
+      gender: ['', Validators.required],
+      dmy: ['', Validators.required], // Data de nascimento
+      weight: ['', [Validators.required, Validators.min(1)]],
+      height: ['', [Validators.required, Validators.min(1)]],
     });
   }
 
+  // Função para ir para o próximo passo
+  goToNextStep() {
+    if (this.registerFormStep1.valid) {
+      this.currentStep = 2;
+    } else {
+      console.log('Formulário da Etapa 1 é inválido');
+    }
+  }
+
+  // Função para finalizar o registro
+  onRegister() {
+    if (this.registerFormStep2.valid) {
+      const fullRegistrationData = {
+        ...this.registerFormStep1.value,
+        ...this.registerFormStep2.value,
+      };
+      console.log('Dados Completos de Registro:', fullRegistrationData);
+
+      // Redireciona para a página principal após o registro
+      this.router.navigate(['/home']);
+    } else {
+      console.log('Formulário da Etapa 2 é inválido');
+    }
+  }
+  
+  // Função para alternar a visibilidade da senha
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
 
-  onRegister() {
-    if (this.registerForm.valid) {
-      console.log('Registration Successful:', this.registerForm.value);
-      // Navegar para a página principal
-      this.router.navigate(['/home']);
-    }
+  // Função auxiliar para determinar se os botões "Próximo" e "Registrar" devem estar habilitados
+  isNextStepDisabled(): boolean {
+    return !this.registerFormStep1.valid;
+  }
+
+  isRegisterDisabled(): boolean {
+    return !this.registerFormStep2.valid;
   }
 }
