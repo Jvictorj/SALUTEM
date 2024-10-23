b753aaadb0ba81e2111dd75e8b1ca80d06c96562
João Vitor Gomes <jvictor.gomes@outlook.com>
2024-10-23 00:11:16 -0300

    exercicios
    pagina de exericios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  selectedDate: string = '';
+  isDatePickerOpen: boolean = false;
+
+  // Definimos o tipo do array weekDays
+  weekDays: { label: string, value: number }[] = [
+    { label: 'Dom', value: 1 },
+    { label: 'Seg', value: 2 },
+    { label: 'Ter', value: 3 },
+    { label: 'Qua', value: 4 },
+    { label: 'Qui', value: 5 },
+    { label: 'Sex', value: 6 },
+    { label: 'Sáb', value: 7 },
+  ];
+
   constructor() {}
+
+  // Definimos o tipo do parâmetro 'day' como o mesmo tipo usado no array weekDays
+  selectDay(day: { label: string, value: number }): void {
+    console.log('Dia selecionado:', day);
+  }
+
+  onAdd() {
+    console.log('Adicionando nova entrada');
+  }
 
 }
