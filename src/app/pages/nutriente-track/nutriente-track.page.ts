4ae22c6eb7ea071f5f6a62c65d3402ed175e1b30
João Vitor Gomes <jvictor.gomes@outlook.com>
2024-11-24 02:58:58 -0300

    api nutri track
    implementação da api nutri track

--- src/app/pages/nutriente-track/nutriente-track.page.ts
+++ src/app/pages/nutriente-track/nutriente-track.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NutrienteService } from '../../service/services/nutriente.service';
 
 @Component({
   selector: 'app-nutriente-track',
   templateUrl: './nutriente-track.page.html',
   styleUrls: ['./nutriente-track.page.scss'],
 })
-export class NutrienteTrackPage implements OnInit {
+export class NutrienteTrackPage {
+  searchQuery: string = '';
+  alimentos: any[] = [];
 
-  constructor() { }
+  constructor(private nutrienteService: NutrienteService) {}
 
-  ngOnInit() {
+  // Buscar alimentos quando o usuário digitar
+  onSearch() {
+    if (this.searchQuery.trim() === '') {
+      this.alimentos = [];
+      return;
+    }
+
+    this.nutrienteService.getAlimentoByName(this.searchQuery).subscribe(
+      (result) => {
+        this.alimentos = result;
+      },
+      (error) => {
+        console.error('Erro ao carregar dados', error);
+      }
+    );
   }
-
 }
